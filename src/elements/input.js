a37407faed04161f0e4a68aae7d61c04b0d6f832
damiiya <ddam2lee@gmail.com>
2022-06-12 14:52:42 +0900

    header 내용 추가 / input 내용 추가

---
+++ src/elements/input.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const input = () => {
+  return (
+    <div class="cntr">
+      <div class="cntr-innr">
+        <label class="search" for="inpt_search">
+          <input id="inpt_search" type="text" />
+        </label>
+        <p>Hover to see the magic.</p>
+      </div>
+    </div>
+  );
+};
+
+export default input;
